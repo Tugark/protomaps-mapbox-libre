3cbfd42808e9a81ecb3d6de8e1765b8ee5ff8625
Lukas Merz <lukemerz@gmail.com>
2023-03-19 12:46:11 +0100

    Add Protomaps

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import * as maplibregl from 'maplibre-gl';
+import maplibregl from 'maplibre-gl';
+import * as pmtiles from 'pmtiles';
+import layers from 'protomaps-themes-base';
 
 @Component({
   selector: 'app-world-map',
@@ -8,11 +10,26 @@ import * as maplibregl from 'maplibre-gl';
 })
 export class WorldMapComponent implements OnInit {
   ngOnInit(): void {
+    let protocol = new pmtiles.Protocol();
+    maplibregl.addProtocol('pmtiles', protocol.tile);
+
     const map = new maplibregl.Map({
       container: 'map',
-      style: 'https://demotiles.maplibre.org/style.json',
+      style: {
+        version: 8,
+        glyphs: 'https://cdn.protomaps.com/fonts/pbf/{fontstack}/{range}.pbf',
+        sources: {
+          protomaps: {
+            type: 'vector',
+            url: 'pmtiles:///assets/maps/data.pmtiles',
+            attribution:
+              '<a href="https://protomaps.com">Protomaps</a> © <a href="https://openstreetmap.org">OpenStreetMap</a>',
+          },
+        },
+        layers: layers('protomaps', 'light'),
+      },
       center: [8.54027, 47.37785],
-      zoom: 7,
+      zoom: 11,
     });
   }
 }
